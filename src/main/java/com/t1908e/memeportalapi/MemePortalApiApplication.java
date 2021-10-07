559a784a3b0098f6ae1b7442462556b6292986d2
huyldth1908041 <huyldth1908041>
2021-10-07 18:58:48 +0700

    init project

---
+++ src/main/java/com/t1908e/memeportalapi/MemePortalApiApplication.java
@@ -0,0 +1,11 @@
+package com.t1908e.memeportalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemePortalApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MemePortalApiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/t1908e/memeportalapi/MemePortalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t1908e.memeportalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemePortalApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
