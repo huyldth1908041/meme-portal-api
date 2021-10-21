4085383fe195b8f8d609c5dca28a0511a9571424
huyldth1908041 <huyldth1908041>
2021-10-21 08:44:46 +0700

    draft commit rebase latter

--- src/main/java/com/t1908e/memeportalapi/dto/CommentDTO.java
+++ src/main/java/com/t1908e/memeportalapi/dto/CommentDTO.java
@@ -1,24 +1,29 @@
 package com.t1908e.memeportalapi.dto;
 
 import com.t1908e.memeportalapi.entity.Comment;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.domain.Page;
 
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class CommentDTO {
     private int id;
     private String content;
     private Date createdAt;
     private Date updatedAt;
     private int status;
-    private int repliedCommentId;
-
     private int postId;
-
     private long userId;
+    private Page<CommentDTO> repliedComments;
+    private int likeCount;
 
-    private Set<CommentLikeDTO> commentLikeDTO;
 
     public CommentDTO(Comment comment) {
         this.id = comment.getId();
@@ -26,11 +31,7 @@ public class CommentDTO {
         this.createdAt = comment.getCreatedAt();
         this.updatedAt = comment.getUpdatedAt();
         this.status = comment.getStatus();
-        this.repliedCommentId = comment.getRepliedCommentId();
         this.postId = comment.getPostId();
         this.userId = comment.getUserId();
-        //convert set<CommentLike> to set<CommentLikeDTO>
-        this.commentLikeDTO = new HashSet<>();
-        comment.getCommentLikes().forEach(item -> this.commentLikeDTO.add(new CommentLikeDTO(item)));
     }
 }
