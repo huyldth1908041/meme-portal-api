e898d3799088541de0ff359ab3ee11129b6cf5d7
huyldth1908041 <huyldth1908041>
2021-11-06 22:10:05 +0700

    update post api

--- src/main/java/com/t1908e/memeportalapi/repository/PostLikeRepository.java
+++ src/main/java/com/t1908e/memeportalapi/repository/PostLikeRepository.java
@@ -2,10 +2,18 @@ package com.t1908e.memeportalapi.repository;
 
 import com.t1908e.memeportalapi.entity.PostLike;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PostLikeRepository extends JpaRepository<PostLike, Integer> {
-    Optional<PostLike> findPostLikeByPostIdAndUserId(int postId,long userId);
+    Optional<PostLike> findPostLikeByPostIdAndUserId(int postId, long userId);
+
+    int countByPostId(int postId);
+
+    @Query(value = "SELECT pl.userId FROM PostLike pl WHERE pl.postId = :postId")
+    List<Long> findListUserIdLiked(@Param(value = "postId") int postId);
 
 }
